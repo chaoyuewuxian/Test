9c1d5982857bcc073b6b2aec340df97d91499f47
zhangruichao <zhangruichao@ucfgroup.com>
2018-05-15 21:49:07 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by zrc on 2018/5/15.
+//  Copyright © 2018年 zrc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
